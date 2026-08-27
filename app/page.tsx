dc4e019c87f9ea49d4832506c95ff59675c55e60
Nikanya Raksasat <6729011007@cdti.ac.th>
2026-08-27 09:59:38 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import BlogCard from "@/components/BlogCard"
+
 
 export default function Home() {
   return (
@@ -8,7 +8,7 @@ export default function Home() {
       <h2>ยินดีต้อนรับเข้าสู่เว็ปไซต์</h2>
       <p>โดย สุวพิชญ์ ปุระวัฒน์</p>
 
-      <BlogCard/>
+      
       
     </div>
   );
